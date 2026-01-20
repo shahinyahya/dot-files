ac55a8dac1728ffdda1a22264b69f4bf5b64f296
shahinyahya <shahin@zennode.com>
2026-01-20 11:12:59 +0530

    removeed hyprland zip to latest update

---
+++ hyprland/dotfiles/hypr/theme/current.bash
@@ -0,0 +1,37 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2025 Aditya Shakya <adi1090x@gmail.com>
+#
+# Current Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#20262C'
+foreground='#f1fcf9'
+color0='#20262c'
+color1='#db86ba'
+color2='#74dd91'
+color3='#e49186'
+color4='#75dbe1'
+color5='#b4a1db'
+color6='#9ee9ea'
+color7='#f1fcf9'
+color8='#465463'
+color9='#d04e9d'
+color10='#4bc66d'
+color11='#db695b'
+color12='#3dbac2'
+color13='#825ece'
+color14='#62cdcd'
+color15='#e0e5e5'
+
+# Wallpaper
+wallpaper="$HOME/.config/hypr/wallpapers/wallpaper.png"
+
+# GTK Theme
+gtk_theme='Manhattan'
+gtk_icons='Luv-Folders-Dark'
+gtk_font='Noto Sans 9'
+cursor_theme='Qogirr-Dark'
+
+# Geany
+geany_colors='manhattan.conf'
